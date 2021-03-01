feabb4df02165561c1a29b25d44e9ff8f4e9aa00
Vsevolod Ivshin <mikemnn78@gmail.com>
2021-03-02 02:08:17 +0700

    Update TestVK.cpp

--- TestVK/TestVK.cpp
+++ TestVK/TestVK.cpp
@@ -6,11 +6,18 @@ int main()
 {
   int n;
   char c;
-  std::cout << "Введите количество искомых повторений:" << std::endl;
+  std::cout << "Enter reuqired amount of repeats:" << std::endl;
   std::cin >> n;
+  while (std::cin.fail() || n <= 0) {
+    std::cout << "Enter correct reuqired amount of repeats(integer >=0):" << std::endl;
+    std::cin.clear();
+    std::cin.ignore(256, '\n');
+    std::cin >> n;
+  }
+
   FirstNRepeatingStream* n_repeat = new FirstNRepeatingStream(n);
   do {
-    std::cout << "Введите следующий символ:" << std::endl;
+    std::cout << "Enter next char:" << std::endl;
     std::cin >> c;
     std::cout << n_repeat->next(c) << std::endl;
   } while (c != '0');
